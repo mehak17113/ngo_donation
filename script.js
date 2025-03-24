cc04e70dd899050885115694fc2f7d7f14a56cba
mehak17113 <mehakgupta5659@gmail.com>
2025-03-24 10:36:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-analytics.js";
+import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyC9P2BhjL0dCOQ0ElDjiEjs5mMn5DHXars",
+    authDomain: "ngo1-f9a6e.firebaseapp.com",
+    projectId: "ngo1-f9a6e",
+    storageBucket: "ngo1-f9a6e.appspot.com",
+    messagingSenderId: "309726258338",
+    appId: "1:309726258338:web:57ae10fa3641ad709763fb",
+    measurementId: "G-K6GFYNZPF7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+
+
+
+
+
+const submit = document.getElementById('submit');
+submit.addEventListener("click", function (event) {
+
+    event.preventDefault()
+
+
+    const password = document.getElementById('pass').value; // Repeated password input value for sign-up
+    const email = document.getElementById('email').value; // Email address input value for sign-up
+
+    createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            // Signed up 
+            const user = userCredential.user;
+            alert("creating...")
+            window.location.href='home1.html';
+            // ...
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            alert(errorMessage)
+            // ..
+        });
+})
+
+
